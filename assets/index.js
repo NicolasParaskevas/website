6fc49ef3bb5976121a2c922bc6afc96b98c3f750
NicolasParaskevas <nicolasparaskevas@hotmail.com>
2021-04-10 21:18:22 +0300

    change indendation from 2 spaces to 4 spaces

--- assets/index.js
+++ assets/index.js
@@ -3,8 +3,8 @@ const windowWidth = window.innerWidth / 5;
 const windowHeight = window.innerHeight / 5 ;
 
 document.addEventListener('mousemove', (e) => {
-  const mouseX = e.clientX / windowWidth;
-  const mouseY = e.clientY / windowHeight;
-  
-  bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+    const mouseX = e.clientX / windowWidth;
+    const mouseY = e.clientY / windowHeight;
+
+    bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
 });
