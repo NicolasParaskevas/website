34cb33d28fdb32f8a2e503f4ce321f6c58ee5bd2
NicolasParaskevas <nicolasparaskevas@hotmail.com>
2021-04-11 12:28:23 +0300

    make all tabs the same height

--- assets/index.js
+++ assets/index.js
@@ -8,3 +8,16 @@ document.addEventListener('mousemove', (e) => {
 
     bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
 });
+
+maxheight = 0;
+tabs = jQuery('.tab-content .tab-pane');
+jQuery.each(tabs, function() {
+    this.classList.add("active"); /* make all visible */
+    maxheight = (this.clientHeight > maxheight ? this.clientHeight : maxheight);
+    if (!jQuery(this).hasClass("show")) {
+        this.classList.remove("active"); /* hide again */
+    }
+});
+jQuery.each(tabs, function() {
+    jQuery(this).height(maxheight);
+});
